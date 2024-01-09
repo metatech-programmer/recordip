d10d20a363d4757d1322c1ee62c7479c186c7782
Santiago Aguilar <97856988+metatech-programmer@users.noreply.github.com>
2024-01-09 01:13:56 -0500

    update app

--- db.php
+++ db.php
@@ -1,17 +1,42 @@
+<!-- POSTGRESS -->
 <?php
+// Configurar el nivel de error para no mostrar advertencias
+error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_RECOVERABLE_ERROR);
 
 $servidor = 'localhost';
-$puerto = '8096';
+$puerto = '5432';
 $baseDeDatos = 'db_app_remembers';
-$usuario = 'php_app';
-$contrasenna = '654321';
+$usuario = 'php_admin';
+$contrasenna = '123456';
+
 
 try {
     $conexion = new PDO("pgsql:host=$servidor;port=$puerto; dbname=$baseDeDatos", $usuario, $contrasenna);
-
 } catch (PDOException $ex) {
     echo "Error al conectar a la base de datos: " . $ex->getMessage();
-   phpinfo();
+    phpinfo();
 }
-
 ?>
+
+
+
+
+<!-- MY SQL 
+<?php /*
+error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_RECOVERABLE_ERROR);
+
+$servidor = 'localhost';
+$puerto = '3306';
+$baseDeDatos = 'id20563866_db_app';
+$usuario = 'id20563866_php_app';
+$contrasenna = 'k[n-v%{8]&b_d^iN';
+try {
+$conexion = new PDO("mysql:host=$servidor;dbname=$baseDeDatos;charset=utf8mb4", $usuario, $contrasenna);
+$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $ex) {
+echo "Error al conectar a la base de datos: " . $ex->getMessage();
+phpinfo();
+}
+*/
+?>
+-->
---
+++ dockerfile
@@ -0,0 +1,20 @@
+# Utilizar la imagen oficial de PHP con Apache
+FROM php:7.4-apache
+
+# Actualizar índices de paquetes y instalar dependencias
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends \
+        libpq-dev \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+# Instalar extensiones de PHP
+RUN docker-php-ext-install pdo pdo_pgsql
+
+# Limpiar el sistema para reducir el tamaño de la imagen
+RUN rm -rf /tmp/* /var/tmp/*
+
+# Exponer el puerto 80 para el servidor Apache
+EXPOSE 80
+
+# CMD ["apache2-foreground"] # Descomentar si no se está utilizando una configuración personalizada de Apache
