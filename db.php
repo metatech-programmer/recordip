64852b57ae80ce359c29940c79c5a1645714e172
metatech-programmer <97856988+metatech-programmer@users.noreply.github.com>
2024-01-17 17:58:40 -0500

    update db

--- db.php
+++ db.php
@@ -1,4 +1,3 @@
-<!-- POSTGRESS -->
 <?php
 // Configurar el nivel de error para no mostrar advertencias
 error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_RECOVERABLE_ERROR);
@@ -9,34 +8,13 @@ $baseDeDatos = 'verceldb';
 $usuario = 'default';
 $contrasenna = 'uY2LiBPEH3eD';
 
+$sslmode = 'require'; // Puedes ajustar esto según tus necesidades
+$endpointId = 'ep-quiet-lake-52744492-pooler'; // Ajusta esto según tu configuración
 
 try {
-    $conexion = new PDO("pgsql:host=$servidor;port=$puerto; dbname=$baseDeDatos", $usuario, $contrasenna);
+    $conexion = new PDO("pgsql:host=$servidor;port=$puerto;dbname=$baseDeDatos;user=$usuario;password=$contrasenna;sslmode=$sslmode;options='endpoint=$endpointId'");
 } catch (PDOException $ex) {
     echo "Error al conectar a la base de datos: " . $ex->getMessage();
     phpinfo();
 }
 ?>
-
-
-
-
-<!-- MY SQL 
-<?php /*
-error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_RECOVERABLE_ERROR);
-
-$servidor = 'localhost';
-$puerto = '3306';
-$baseDeDatos = 'id20563866_db_app';
-$usuario = 'id20563866_php_app';
-$contrasenna = 'k[n-v%{8]&b_d^iN';
-try {
-$conexion = new PDO("mysql:host=$servidor;dbname=$baseDeDatos;charset=utf8mb4", $usuario, $contrasenna);
-$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $ex) {
-echo "Error al conectar a la base de datos: " . $ex->getMessage();
-phpinfo();
-}
-*/
-?>
--->
